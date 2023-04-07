4dc82610fa5d14995a7e5fdeda20483f1a7e8530
Summer <72228259+SummerinTech@users.noreply.github.com>
2023-04-07 13:50:09 -0700

    Style Header component
    Create different style components in header for Nav, Logo, and Button. Button is reusable.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 const Header = () => (
 	<StyledHeader>
 		<Container>
-			<h1>Hubble</h1>
+			<Nav>
+				<Logo src="./images/logo.svg" />
+				<Button>Try It Free</Button>
+			</Nav>
 		</Container>
 	</StyledHeader>
 );

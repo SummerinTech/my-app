7336d97a2ccf17e21515f2cb9fc864806e2a54d7
Summer <72228259+SummerinTech@users.noreply.github.com>
2023-04-07 11:02:46 -0700

    Create StyledHeader
    Create a styled header and link it to the styled header component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
 
 const App = () => {
-	return <Container>Hello world.</Container>;
+	return (
+		<>
+			<Header />
+			<Container>Hello world.</Container>
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => (
+	<StyledHeader>
+		<h1>Hubble</h1>
+	</StyledHeader>
+);
+
+export default Header;
